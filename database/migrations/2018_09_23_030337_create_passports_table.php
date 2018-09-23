c12157f55fe8013b37906ae37b2d8ce403a943a2
Seongmin <camelmevius@gmail.com>
2018-09-23 12:38:14 +0900

    add

--- database/migrations/2018_09_23_030337_create_passports_table.php
+++ database/migrations/2018_09_23_030337_create_passports_table.php
@@ -14,8 +14,14 @@ class CreatePassportsTable extends Migration
     public function up()
     {
         Schema::create('passports', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+          $table->increments('id');
+          $table->string('name');
+          $table->integer('date');
+          $table->string('email')->unique();
+          $table->integer('number');
+          $table->string('office');
+          $table->string('filename');
+          $table->timestamps();
         });
     }
 
