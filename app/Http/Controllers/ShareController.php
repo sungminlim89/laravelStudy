1d9814b01b32e006294a5c3c5a0bc5ec42832076
sungminlim89 <sungminlim8909@gmail.com>
2018-11-10 00:32:33 +0900

    functions

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -13,7 +13,9 @@ class ShareController extends Controller
    */
   public function index()
   {
-      //
+      $shares = Share::all();
+
+      return view('shares.index', compact('shares'));
   }
 
   /**
@@ -34,7 +36,18 @@ class ShareController extends Controller
    */
   public function store(Request $request)
   {
-      //
+    $request->validate([
+      'share_name'=>'required',
+      'share_price'=> 'required|integer',
+      'share_qty' => 'required|integer'
+    ]);
+    $share = new Share([
+      'share_name' => $request->get('share_name'),
+      'share_price'=> $request->get('share_price'),
+      'share_qty'=> $request->get('share_qty')
+    ]);
+    $share->save();
+    return redirect('/shares')->with('success', 'Stock has been added');
   }
 
   /**
@@ -56,7 +69,9 @@ class ShareController extends Controller
    */
   public function edit($id)
   {
-      //
+      $share = Share::find($id);
+
+      return view('shares.edit', compact('share'));
   }
 
   /**
@@ -68,7 +83,19 @@ class ShareController extends Controller
    */
   public function update(Request $request, $id)
   {
-      //
+      $request->validate([
+        'share_name'=>'required',
+        'share_price'=> 'required|integer',
+        'share_qty' => 'required|integer'
+      ]);
+
+      $share = Share::find($id);
+      $share->share_name = $request->get('share_name');
+      $share->share_price = $request->get('share_price');
+      $share->share_qty = $request->get('share_qty');
+      $share->save();
+
+      return redirect('/shares')->with('success', 'Stock has been updated');/
   }
 
   /**
@@ -79,6 +106,9 @@ class ShareController extends Controller
    */
   public function destroy($id)
   {
-      //
+    $share = Share::find($id);
+    $share->delete();
+
+    return redirect('/shares')->with('success', 'Stock has been deleted Successfully');
   }
 }
