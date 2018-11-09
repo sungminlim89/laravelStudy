43f6e7809adb74444588eef0795492e19fc97a7f
sungminlim89 <sungminlim8909@gmail.com>
2018-11-09 23:38:21 +0900

    a

--- app/Share.php
+++ app/Share.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Share extends Model
 {
-  'share_name',
-  'share_price',
-  'share_qty'
+  protected $fillable = [
+    'share_name',
+    'share_price',
+    'share_qty'
+  ];
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('shares', 'ShareController');
+Route::resource('products', 'ProductController');
+Route::resource('shares', 'ShareController')
