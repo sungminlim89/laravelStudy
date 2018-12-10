41a43684cbf588eb09e94394ec1acb3d942f5c29
root <root@ip-172-31-47-39.ap-northeast-1.compute.internal>
2018-12-10 14:10:35 +0000

    complete

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('products.index');
+// });
+Route::get('/', 'ProductController@index');
+//Route::get('/create', 'ProductController@create');
 
 Route::resource('products','ProductController');
